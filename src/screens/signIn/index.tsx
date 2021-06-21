c4b9461932b1d80bfe56663f32589536b51ec9c3
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2021-06-21 19:58:37 -0300

    feat: started project

---
+++ src/screens/signIn/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+
+import IllustrationImg from '../../assets/illustration.png';
+import { styles } from './styles';
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      
+      <Image source={IllustrationImg} />
+    </View>
+  );
+}
