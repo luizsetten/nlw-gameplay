452983991c97f019ec56fcb780cf5db4db71b3bb
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2021-06-21 21:14:23 -0300

    feat: finish signIn layout

--- src/screens/signIn/index.tsx
+++ src/screens/signIn/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Image, StatusBar } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 import { ButtonIcon } from '../../components/ButtonIcon'
 import IllustrationImg from '../../assets/illustration.png';
@@ -8,11 +8,6 @@ import { styles } from './styles';
 export function SignIn() {
   return (
     <View style={styles.container}>
-      <StatusBar 
-        barStyle="light-content" 
-        backgroundColor="transparent" 
-        translucent />
-
       <Image 
         source={IllustrationImg} 
         style={styles.image} 
@@ -20,9 +15,9 @@ export function SignIn() {
 
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize {"\n"} 
-          suas jogatinas {"\n"} 
-          facilmente
+          Conecte-se {"\n"} 
+          e orgazine suas {"\n"} 
+          jogatinas
         </Text>
 
         <Text style={styles.subtitle}>
