fd39fb04e7909ce303b42d783610645f8919b1e2
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2021-06-23 17:51:28 -0300

    feat: done modal

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -14,9 +14,23 @@ import { GuildIcon } from '../../components/GuildIcon';
 import { SmallInput } from '../../components/SmallInput';
 import { TextArea } from '../../components/TextArea';
 import { Button } from '../../components/Button';
+import { ModalView } from '../../components/ModalView';
+import { Guilds } from '../Guilds';
+import { GuildProps } from '../../components/Appointment';
 
 export function AppointmentCreate() {
   const [category, setCategory] = useState('');
+  const [openGuildsModal, setOpenGuildsModal] = useState(false);
+  const [guild, setGuild] = useState<GuildProps>({} as GuildProps);
+
+  function handleOpenGuilds() {
+    setOpenGuildsModal(true);
+  }
+
+  function handleGuildSelect(guildSelected: GuildProps) {
+    setGuild(guildSelected);
+    setOpenGuildsModal(false);
+  }
 
   return(
     <KeyboardAvoidingView 
@@ -42,16 +56,21 @@ export function AppointmentCreate() {
             />
 
           <View style={styles.form}>
-            <RectButton>
+            <RectButton onPress={handleOpenGuilds}>
               <View style={styles.select}>
                 {
-                  // <View style={styles.image}/>
-                  <GuildIcon />
+                  guild.icon 
+                  ? <GuildIcon /> 
+                  : <View style={styles.image}/>
                 }
 
                 <View style={styles.selectBody}>
                   <Text style={styles.label}>
-                    Selecione um servidor
+                    {
+                    guild.name
+                    ? guild.name 
+                    : 'Selecione um servidor'
+                    }
                   </Text>
                 </View>
 
@@ -105,6 +124,10 @@ export function AppointmentCreate() {
 
         </Background>
       </ScrollView>
+
+      <ModalView visible={openGuildsModal}>
+        <Guilds handleGuildSelet={handleGuildSelect}/>
+      </ModalView>
     </KeyboardAvoidingView>
   )
 }
---
+++ src/screens/Guilds/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { View, FlatList } from "react-native";
+import { Guild, GuildProps } from "../../components/Guild";
+import { ListDivider } from "../../components/ListDivider";
+
+import { styles } from "./styles";
+
+type Props = {
+  handleGuildSelet: (guildSelected: GuildProps) => void
+}
+
+export function Guilds({ handleGuildSelet }: Props) {
+  const guilds = [
+    {
+      id: '1',
+      name: 'lendarios',
+      icon: "null",
+      owner: true
+    }
+  ]
+
+  return(
+    <View style={styles.container}>
+      <FlatList
+        data={guilds}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <Guild  
+            data={item}
+            onPress={() => handleGuildSelet(item)}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ItemSeparatorComponent={() => <ListDivider />}
+        style={styles.guilds}
+      />
+    </View>
+  )
+}
