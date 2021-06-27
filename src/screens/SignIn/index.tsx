6203bf66dd68ef2345de1bc4145fcd9b2bc128d7
Luiz Gustavo Setten <luizgustavosetten@hotmail.com>
2021-06-27 12:29:28 -0300

    feat: insert context

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Text, View, ScrollView, Image } from 'react-native';
-
-import { ButtonIcon } from '../../components/ButtonIcon'
-import IllustrationImg from '../../assets/illustration.png';
-import { styles } from './styles';
 import { useNavigation } from '@react-navigation/native';
-import { Background } from '../../components/Background';
 
+import IllustrationImg from '../../assets/illustration.png';
+
+import { Background } from '../../components/Background';
+import { ButtonIcon } from '../../components/ButtonIcon';
+
+import { useAuth } from '../../hooks/auth';
+
+import { styles } from './styles';
 
 export function SignIn() {
   const navigation = useNavigation();
+
+  const { user } = useAuth();
+  console.log(user)
 
   function handleSignIn() {
     navigation.navigate('Home');
