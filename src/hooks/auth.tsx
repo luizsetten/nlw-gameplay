166063b2203fb7545c39ba422d2844b72873c5b6
Luiz Gustavo Setten <luizgustavosetten@hotmail.com>
2021-06-28 16:08:56 -0300

    feat: remove sensitive information

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -8,12 +8,13 @@ import React, {
 
  import * as AuthSession from 'expo-auth-session';
 
- import { 
-  CDN_IMAGE,
-  CLIENT_ID,
-  REDICRECT_URI,
-  RESPONSE_TYPE,SCOPE
- } from '../config';
+
+const { CDN_IMAGE } = process.env;
+const { CLIENT_ID } = process.env;
+const { REDICRECT_URI } = process.env;
+const { RESPONSE_TYPE } = process.env;
+const { SCOPE } = process.env;
+
 import { api } from '../services/api';
 
 type User = {
@@ -37,7 +38,8 @@ type AuthProviderProps = {
 
 type AuthorizationResponse = AuthSession.AuthSessionResult & {
   params: {
-    access_token: string;
+    access_token?: string;
+    error?: string;
   }
 }
 
@@ -55,26 +57,23 @@ function AuthProvider({ children }: AuthProviderProps) {
 
       const { type, params } = await AuthSession.startAsync({ authUrl }) as AuthorizationResponse;
 
-      if(type === 'success') {
+      if(type === 'success' && !params.error) {
         api.defaults.headers.authorization = `Bearer ${params.access_token}`;
 
         const userInfo = await api.get('/users/@me');
         const firstName = userInfo.data.username.split(' ')[0];
         userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`;
 
-
         setUser({
           ...userInfo.data,
           firstName,
           token: `${params.access_token}`
         });
-
-        setLoading(false);
-      } else {
-        setLoading(false);
-      }
+      } 
     } catch (error) {
       throw new Error('Não foi possível autenticar!');
+    } finally {
+      setLoading(false);
     }
   } 
 
